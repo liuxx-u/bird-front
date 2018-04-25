4410b2da808b502fa3c849b33a3eafd903ed1ed1
liuxx <381334617@qq.com>
2018-04-25 16:21:46 +0800

    修复antd主题less覆盖失败的BUG

--- theme.config.js
+++ theme.config.js
@@ -1,5 +1,9 @@
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
-module.exports = {
-  // 'primary-color': '#10e99b',
-  'card-actions-background': '#f5f8fa',
+const fs = require('fs');
+const path = require('path');
+const lessToJs = require('less-vars-to-js');
+
+module.exports = () => {
+  const themePath = path.join(__dirname, './src/themes/default.less'); //导入覆盖样式的less
+  return lessToJs(fs.readFileSync(themePath, 'utf8'));
 };

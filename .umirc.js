28d3e3e7f9117293bd37d6d371b0cdeb2e0ff03a
liuxx <381334617@qq.com>
2018-09-06 09:14:50 +0800

    升级umi至2.0.2

--- .umirc.js
+++ .umirc.js
@@ -1,25 +1,48 @@
+import { resolve } from 'path';
 
 export default {
+  // for query-string@6 https://github.com/sorrycc/blog/issues/68
+  es5ImcompatibleVersions: true,
   plugins: [
-    'umi-plugin-dva',
     [
-      'umi-plugin-routes',
+      'umi-plugin-react',
       {
-        exclude: [
-          /model\.(j|t)sx?$/,
-          /service\.(j|t)sx?$/,
-          /models\//,
-          /components\//,
-          /services\//
-        ],
-      },
-    ],
-    [
-      'umi-plugin-dll',
-      {
-        exclude: [],
-        include: ["dva", "dva/router", "dva/saga", "dva/fetch", "antd/es"],
-      },
-    ],
+        dva: true,
+        antd: true,
+        routes: {
+          exclude: [
+            /model\.(j|t)sx?$/,
+            /service\.(j|t)sx?$/,
+            /models\//,
+            /components\//,
+            /services\//
+          ]
+        },
+        dll: {
+          exclude: [],
+          include: ["dva", "dva/router", "dva/saga", "dva/fetch", "antd/es"]
+        }
+      }
+    ]
+  ],
+  theme: "./theme.config.js",
+  proxy: {
+    // "/api/v1/weather": {
+    //   "target": "https://api.seniverse.com/",
+    //   "changeOrigin": true,
+    //   "pathRewrite": { "^/api/v1/weather": "/v3/weather" }
+    // }
+  },
+  alias: {
+    themes: resolve(__dirname, './src/themes'),
+    components: resolve(__dirname,"./src/components"),
+    utils: resolve(__dirname,"./src/utils"),
+    config: resolve(__dirname,"./src/utils/config"),
+    services: resolve(__dirname,"./src/services"),
+    models: resolve(__dirname,"./src/models"),
+    routes: resolve(__dirname,"./src/routes"),
+  },
+  urlLoaderExcludes: [
+    /\.svg$/,
   ],
 }
